e4ae90ef1d45ad6fba0cb0917a1bfe1cc700defc
AberBluehair <68063543+AberBluehair@users.noreply.github.com>
2020-11-23 14:05:57 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+//Create variables here
+var dog,dogpic,happyDog,database,foodS,foodStock
+function preload()
+{
+ dogpic = loadImage("images/dogImg.png");
+ happyDog = loadImage("images/dogImg1.png");
+}
+
+function setup() {
+	createCanvas(500,500);
+  dog = createSprite(250,250,60,60)
+  dog.addImage(dogpic)
+  database = firebase.database()
+  foodStock=database.ref("Food");
+  foodStock.on("value",readStock);
+}
+
+
+function draw() {  
+background(46,139,87);
+if(keyWentDown(UP_ARROW)){
+  writeStock(foodS);
+  dog.addImage(happyDog)
+}
+  drawSprites();
+
+  //add styles here
+
+}
+function readStock(data){
+  foodS = data.val();
+}
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+  database.ref('/').update({Food:x})
+}
+
